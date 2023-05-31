ff600d1fa7071702627c7596fdf6e58b1011a0d5
Ben Lentschig <134686333+BenLent123@users.noreply.github.com>
2023-05-31 14:45:11 +0200

    Add files via upload

---
+++ sketch_d_encoedr.ino
@@ -0,0 +1,78 @@
+#include <Servo.h> 
+#define REC 8
+#define LEC 7
+Servo servoLeft;                             
+Servo servoRight;
+
+int counter = 0;
+int currstateLEC;
+int laststateLEC;
+int currstateREC;
+int laststateREC;
+String currentDir ="";
+
+void setup() {
+  Serial.begin (9600);
+pinMode(REC,INPUT);
+pinMode(LEC,INPUT);
+laststateLEC = digitalRead(LEC);
+laststateREC = digitalRead(REC);
+}
+
+void loop() {
+encode();
+forward();
+
+}
+
+void spinR() {
+  servoLeft.attach(13);                   
+  servoRight.attach(12);
+  servoLeft.writeMicroseconds(1700);         
+  servoRight.writeMicroseconds(1700);
+} 
+
+void spinL() {
+  servoLeft.attach(13);                   
+  servoRight.attach(12);
+  servoLeft.writeMicroseconds(1300);         
+  servoRight.writeMicroseconds(1300);
+}  
+
+
+void forward() {
+  servoLeft.attach(13);                   
+  servoRight.attach(12);
+  servoLeft.writeMicroseconds(1300);         
+  servoRight.writeMicroseconds(1700);
+}    
+
+void backward() {
+  servoLeft.attach(13);                   
+  servoRight.attach(12);
+  servoLeft.writeMicroseconds(1700);         
+  servoRight.writeMicroseconds(1300);
+}   
+
+void stop() {
+  servoLeft.detach();
+  servoRight.detach();
+}  
+
+void encode(){
+  // Read the current state of CLK
+	currstateLEC = digitalRead(LEC);
+
+	// If last and current state of CLK are different, then pulse occurred
+	// React to only 1 state change to avoid double count
+	if (currstateLEC != laststateLEC  && currstateLEC == 1){
+			counter ++;
+			currentDir ="CW";
+		}
+
+		Serial.print("Direction: ");
+		Serial.print(currentDir);
+		Serial.print(" | Counter: ");
+		Serial.println(counter);
+	}
+
