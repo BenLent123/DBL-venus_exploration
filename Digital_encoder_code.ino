272545ecae206c92ac986b17db03dbca52082078
Ben Lentschig <134686333+BenLent123@users.noreply.github.com>
2023-06-02 21:09:36 +0200

    Update Digital_encoder_code.ino

--- Digital_encoder_code.ino
+++ Digital_encoder_code.ino
@@ -1,39 +1,65 @@
 #include <Servo.h>
 #define LENCODER 8
 #define RENCODER 7
-int angles=0;
-int numberOfHoles = 0;
-bool prevreader;
-bool prevdreader;
-int disr=(((numberOfHoles)*3.14*6.5)/24);
-int disl=(((angles)*3.14*6.5)/24);
+int nholesR =0;
+int nholesL = 0;
+int rotationsR = 0;
+int rotationsL = 0;
+bool prevreadL;
+bool prevreadR;
 
 void setup() {
-  pinMode(LENCODER, INPUT);                        //encoder left
-  pinMode(RENCODER, INPUT); 
-      
+  pinMode(LENCODER, INPUT);                   
+  pinMode(RENCODER, INPUT);
 }
+void loop(){}
 
-void loop(){
-  
-}
-
-void readEncoders() {
+int leftencoder() {
   boolean readwheel = digitalRead(LENCODER);                  //read encoders
-  if (readwheel == 1 && readwheel != prevreader) {                  //make sure that it is not the same hole being read
-    numberOfHoles += 1;
-    prevreader = 1;
+  if (readwheel == 1 && readwheel != prevreadL) {             //make sure that it is not the same hole being read
+    nholesL += 1;
+    prevreadL = 1;
   } else if (readwheel == 0) {
-    prevreader = 0;
+    prevreadL = 0;
   }
+  if (nholesL = 8){
+   nholesL = 0;
+   rotationsL++:
+  }
+  
+  int distance = (((rotationsL)*3.14*6.5)/24);
+  return(distance);
+
 }
 
-void readangles() {
+int rightencoder() {
   boolean readwheel = digitalRead(RENCODER);                  //read encoders
-  if (readwheel == 1 && readwheel != prevdreader) {                  //make sure that it is not the same hole being read
-    angles += 1;
-    prevdreader = 1;
+  if (readwheel == 1 && readwheel != prevreadR) {             //make sure that it is not the same hole being read
+    nholesR += 1;
+    prevreadR = 1;
   } else if (readwheel == 0) {
-    prevdreader = 0;
+    prevreadR = 0;
   }
+  if (nholesR = 8){
+   nholesR = 0;
+   rotationsR++:
+  }
+  
+  int distance = (((rotationsR)*3.14*6.5)/24);
+  return(distance);
 }
+
+// TRYOUT?!?!?!
+/*
+int angle(){
+int angR = rightencoder();
+int angL = leftencoder();
+
+if (angR > angL){
+return(1);
+}else if (angR < angL) {
+return(2);
+}
+return(-1);
+}
+*/
